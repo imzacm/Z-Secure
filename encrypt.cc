9624acb8233af7fcebcde4f61301d1cbdb6dcceb
imzacm <imzacm@gmail.com>
2016-03-29 14:13:51 +0100

    Create encrypt.cc

---
+++ encrypt.cc
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <string>
+#include <iostream>
+#include "crypt.h"
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  
+}
